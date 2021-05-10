257e2bf49abe103dac320c269051509cde51bbbc
Jasur Ahmadov <cesurehmedov594@gmail.com>
2021-05-11 01:33:01 +0400

    my project

---
+++ ShopDataBaseDesktopApp/src/shopdatabasedesktopapp/ShopDataBaseDesktopApp.java
@@ -0,0 +1,9 @@
+package shopdatabasedesktopapp;
+
+public class ShopDataBaseDesktopApp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+    }
+}
